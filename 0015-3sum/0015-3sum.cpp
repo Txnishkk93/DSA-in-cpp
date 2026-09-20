46c445a5cb1e98189879faf840d05230cbd09e0e
Tanishk rajput <rajputt4ni5hk@gmail.com>
2026-09-20 22:20:34 +0530

    Time: 44 ms (73.39%), Space: 28.9 MB (92.4%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -5,30 +5,27 @@ public:
         sort(nums.begin(), nums.end());
 
         for (int i = 0; i < nums.size(); i++) {
-            if (i > 0 && nums[i] == nums[i-1]) {
+            if (i > 0 && nums[i] == nums[i - 1])
                 continue;
-            }
-            
+
             int j = i + 1;
             int k = nums.size() - 1;
-
             while (j < k) {
-                int total = nums[i] + nums[j] + nums[k];
-
-                if (total > 0) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum > 0) {
                     k--;
-                } else if (total < 0) {
+                } else if (sum < 0) {
                     j++;
                 } else {
                     res.push_back({nums[i], nums[j], nums[k]});
                     j++;
+                    k--;
 
-                    while (nums[j] == nums[j-1] && j < k) {
+                    while (j < k && nums[j] == nums[j - 1])
                         j++;
-                    }
                 }
             }
         }
-        return res;        
+        return res;
     }
 };
